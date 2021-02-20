06118cc4857782ac964a45136228069aeab70145
Vitor Carlos <vitorcarlos1312@gmail.com>
2021-02-20 08:23:58 -0300

    create css from page home

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import SignIn from './pages/Signin/index';
+import GlobalStyle from './styles/global';
+
+const App: React.FC = () => (
+  <>
+    <SignIn />
+    <GlobalStyle />
+  </>
+);
+
+export default App;
