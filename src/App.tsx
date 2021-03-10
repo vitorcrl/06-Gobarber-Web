7d6454f94711683071e48ca9098df45b3d1efa07
Vitor Carlos <vitorcarlos1312@gmail.com>
2021-03-09 21:20:22 -0300

    create connection from context

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,13 @@ import SignIn from './pages/Signin/index';
 // import SignUp from './pages/SignUp/index';
 import GlobalStyle from './styles/global';
 
-import AuthContext from './context/AuthContext';
+import { AuthProvider } from './context/AuthContext';
 
 const App: React.FC = () => (
   <>
-    <AuthContext.Provider value={{ name: 'Vitor' }}>
+    <AuthProvider>
       <SignIn />
-    </AuthContext.Provider>
+    </AuthProvider>
 
     <GlobalStyle />
   </>
